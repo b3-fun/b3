3265a425b0fd174cc085a214890516ae451f8554
An <anqtran36@gmail.com>
2025-07-07 08:49:42 -0400

    addressed comments

--- packages/sdk/src/global-account/react/hooks/useTokenFromUrl.tsx
+++ packages/sdk/src/global-account/react/hooks/useTokenFromUrl.tsx
@@ -4,7 +4,6 @@ import { Token } from "@b3dotfun/sdk/anyspend";
 import { getCoingeckoChainInfo } from "@b3dotfun/sdk/shared/constants/chains/supported";
 import { useQuery } from "@tanstack/react-query";
 import { useSearchParams } from "next/navigation";
-import { fetchTokenInfo } from "./token";
 
 interface UseTokenFromUrlOptions {
   /**
@@ -16,6 +15,37 @@ interface UseTokenFromUrlOptions {
    * The URL parameter prefix to look for (e.g., "from" or "to")
    */
   prefix: string;
+}
+interface TokenInfo {
+  data: {
+    attributes: {
+      address: string;
+      name: string;
+      symbol: string;
+      decimals: number;
+      image_url: string;
+    };
+  };
+}
+
+async function fetchTokenInfo(network: string, address: string): Promise<TokenInfo> {
+  const response = await fetch("https://api.b3.fun/tokens", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Service-Method": "getCoinGeckoTokenInfo"
+    },
+    body: JSON.stringify({
+      network,
+      address
+    })
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch token info");
+  }
+
+  return response.json();
 }
 
 /**
