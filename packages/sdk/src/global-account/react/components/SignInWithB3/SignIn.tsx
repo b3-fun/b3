09a004d4b2dda258f0505aa6d6a1759cdcd13a21
Gio Martinez <gio@giologist.com>
2025-07-08 21:25:49 +0200

    mobile fixes & style changes

--- packages/sdk/src/global-account/react/components/SignInWithB3/SignIn.tsx
+++ packages/sdk/src/global-account/react/components/SignInWithB3/SignIn.tsx
@@ -15,7 +15,7 @@ import { ReactNode, useEffect } from "react";
 import { useConnectedWallets, useSetActiveWallet, useWalletInfo } from "thirdweb/react";
 import { ManageAccountButton } from "../custom/ManageAccountButton";
 
-type UserProps = {
+type SignInProps = {
   className?: string;
   isMobile?: boolean;
 };
@@ -25,9 +25,9 @@ type SignInWithB3Props = Omit<SignInWithB3ModalProps, "type" | "showBackButton">
   loggedInButtonText?: string | ReactNode;
   loadingButtonText?: string | ReactNode;
   withLogo?: boolean;
-} & UserProps;
+} & SignInProps;
 
-export function User(props: SignInWithB3Props) {
+export function SignIn(props: SignInWithB3Props) {
   const { className } = props;
   const { automaticallySetFirstEoa } = useB3();
   const {
@@ -41,8 +41,6 @@ export function User(props: SignInWithB3Props) {
     smartWalletIcon,
     eoaWalletIcon
   } = useAccountWallet();
-
-  console.log("@@isActiveSmartWallet", isActiveSmartWallet);
 
   const isMobile = useIsMobile();
   const { logout } = useAuthentication(String(process.env.NEXT_PUBLIC_GLOBAL_ACCOUNTS_PARTNER_ID));
@@ -105,71 +103,73 @@ export function User(props: SignInWithB3Props) {
                 className="b3-root shadow-depth-1 absolute -right-4 top-full min-w-64 rounded-2xl border lg:right-0"
                 modal={false}
                 // TODO: Figure out why setting anchor on mobile causes z-index issues where it appears under elements
-                anchor={isMobile ? "top start" : undefined}
+                anchor={isMobile ? "top end" : undefined}
               >
-                {connectedEOAWallet ? (
-                  <div
-                    className={cn(
-                      "border-b3-react-subtle bg-b3-react-background flex cursor-pointer items-center justify-between rounded-xl p-3",
-                      "bg-b3-react-background hover:bg-theme-on-surface-2"
-                    )}
-                    onClick={() => handleSetActiveAccount(connectedEOAWallet?.id)}
-                  >
-                    <div className="flex items-center">
-                      <img
-                        className="bg-theme-primary h-16 w-16 rounded-full opacity-100"
-                        src={eoaWalletIcon}
-                        alt={connectedEOAWallet?.id}
-                      />
-                      <div className="ml-4 grow">
-                        {ensName && <div className="text-title-1s">{ensName}</div>}
-                        <div className="text-title-1s">{truncateAddress(globalAddress)}</div>
-                        <div className="text-body-1m text-theme-secondary">{walletInfo?.name}</div>
-                      </div>
-                    </div>
-                    {isActiveEOAWallet && <Icon className="fill-theme-primary" name="check" />}
-                  </div>
-                ) : (
-                  connectedSmartWallet && (
+                <div className="bg-b3-react-background">
+                  {connectedEOAWallet ? (
                     <div
                       className={cn(
-                        "mb-2 flex cursor-pointer items-center justify-between rounded-xl p-3",
-                        isActiveSmartWallet ? "bg-theme-n-8" : "bg-b3-react-background hover:bg-theme-on-surface-2"
+                        "border-b3-react-subtle bg-b3-react-background flex cursor-pointer items-center justify-between rounded-xl p-3",
+                        "hover:bg-theme-on-surface-2"
                       )}
-                      onClick={() => handleSetActiveAccount(connectedSmartWallet?.id)}
+                      onClick={() => handleSetActiveAccount(connectedEOAWallet?.id)}
                     >
                       <div className="flex items-center">
                         <img
                           className="bg-theme-primary h-16 w-16 rounded-full opacity-100"
-                          src={smartWalletIcon}
-                          alt={connectedSmartWallet?.id}
+                          src={eoaWalletIcon}
+                          alt={connectedEOAWallet?.id}
                         />
-                        <div className="pl-4.5 grow">
+                        <div className="ml-4 grow">
                           {ensName && <div className="text-title-1s">{ensName}</div>}
                           <div className="text-title-1s">{truncateAddress(globalAddress)}</div>
-                          <div className="text-body-1m text-theme-secondary">Smart wallet</div>
+                          <div className="text-body-1m text-theme-secondary">{walletInfo?.name}</div>
                         </div>
                       </div>
-                      {isActiveSmartWallet && <Icon className="fill-theme-primary" name="check" />}
+                      {isActiveEOAWallet && <Icon className="fill-theme-primary" name="check" />}
                     </div>
-                  )
-                )}
+                  ) : (
+                    connectedSmartWallet && (
+                      <div
+                        className={cn(
+                          "mb-2 flex cursor-pointer items-center justify-between rounded-xl p-3",
+                          isActiveSmartWallet ? "bg-theme-n-8" : "bg-b3-react-background hover:bg-theme-on-surface-2"
+                        )}
+                        onClick={() => handleSetActiveAccount(connectedSmartWallet?.id)}
+                      >
+                        <div className="flex items-center">
+                          <img
+                            className="bg-theme-primary h-16 w-16 rounded-full opacity-100"
+                            src={smartWalletIcon}
+                            alt={connectedSmartWallet?.id}
+                          />
+                          <div className="pl-4.5 grow">
+                            {ensName && <div className="text-title-1s">{ensName}</div>}
+                            <div className="text-title-1s">{truncateAddress(globalAddress)}</div>
+                            <div className="text-body-1m text-theme-secondary">Smart wallet</div>
+                          </div>
+                        </div>
+                        {isActiveSmartWallet && <Icon className="fill-theme-primary" name="check" />}
+                      </div>
+                    )
+                  )}
 
-                <div className="ml-3">
-                  <ManageAccountButton props={props} />
-                </div>
-
-                <button className="mb-2 w-full space-y-1" onClick={onDisconnect}>
-                  <div className="hover:bg-theme-on-surface-2 group flex h-12 items-center rounded-xl px-4 transition-colors">
-                    <Icon
-                      className="fill-theme-secondary group-hover:fill-theme-primary mr-4 shrink-0 transition-colors"
-                      name="logout"
-                    />
-                    <div className="text-base-1s text-theme-secondary group-hover:text-theme-primary mr-auto transition-colors">
-                      Disconnect
-                    </div>
+                  <div className="ml-3">
+                    <ManageAccountButton {...props} className="w-[calc(100%-12px)]" />
                   </div>
-                </button>
+
+                  <button className="mb-2 w-full space-y-1" onClick={onDisconnect}>
+                    <div className="hover:bg-theme-on-surface-2 group flex h-12 items-center rounded-xl px-4 transition-colors">
+                      <Icon
+                        className="fill-theme-secondary group-hover:fill-theme-primary mr-4 shrink-0 transition-colors"
+                        name="logout"
+                      />
+                      <div className="text-base-1s text-theme-secondary group-hover:text-theme-primary mr-auto transition-colors">
+                        Disconnect
+                      </div>
+                    </div>
+                  </button>
+                </div>
               </MenuItems>
             </Transition>
           </>
@@ -187,4 +187,4 @@ export function User(props: SignInWithB3Props) {
   );
 }
 
-export default User;
+export default SignIn;
