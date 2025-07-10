ad3847de12f11ba39bf3bb93a0d99342dad9d010
nhd98z <nhd98z@gmail.com>
2025-07-10 16:19:25 +0700

    Prettier

--- packages/sdk/src/anyspend/react/components/common/Warning.tsx
+++ packages/sdk/src/anyspend/react/components/common/Warning.tsx
@@ -1,6 +1,6 @@
 export function Warning({ text }: { text: string }) {
   return (
-    <div className="relative rounded border border-orange-400 bg-as-red/60 px-4 py-3 text-white" role="alert">
+    <div className="bg-as-red/60 relative rounded border border-orange-400 px-4 py-3 text-white" role="alert">
       <div className="font-bold">{text}</div>
     </div>
   );
