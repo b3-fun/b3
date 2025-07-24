85b15fa4c87bbd89a5aff8d405a7ce166c3bfb87
nhd98z <34548156+nhd98z@users.noreply.github.com>
2025-07-24 21:32:43 +0700

    Remove dialog rotation transform (#77)

--- packages/sdk/src/global-account/react/components/ui/dialog.tsx
+++ packages/sdk/src/global-account/react/components/ui/dialog.tsx
@@ -54,7 +54,6 @@ const DialogContent: React.ForwardRefExoticComponent<DialogContentProps & React.
               "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 duration-500",
               "data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95",
               "data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]",
-              "data-[state=closed]:[transform:rotateX(10deg)] data-[state=open]:[transform:rotateX(0deg)]",
               "[perspective:1200px] [transform-style:preserve-3d] sm:rounded-xl",
               "transition-all ease-out",
               className,
