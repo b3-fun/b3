3ce8195bf57f08748b8965b17a0a7841d7381d4a
Trung <trung@npclabs.org>
2025-08-15 21:25:15 +0700

    ~Change rpc

--- packages/sdk/src/bondkit/constants.ts
+++ packages/sdk/src/bondkit/constants.ts
@@ -2,5 +2,4 @@ import type { Address } from "viem";
 
 export const BaseBondkitTokenFactoryContractAddress: Address = "0xaAFa26dE2a1be3d8F18D3D051571123d4438F567";
 
-export const BaseMainnetRpcUrl =
-  "https://chaotic-sly-layer.base-mainnet.quiknode.pro/2149979b0c3a32d5b7090a6a0c137904421c3ee0/";
+export const BaseMainnetRpcUrl = "https://base-rpc.publicnode.com";
