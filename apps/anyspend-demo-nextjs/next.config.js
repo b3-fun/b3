358efb0ad905d7239b1ddce255e93f45cf8d3f16
nhd98z <nhd98z@gmail.com>
2025-08-16 22:28:23 +0700

    Update onramp payment UI

--- apps/anyspend-demo-nextjs/next.config.js
+++ apps/anyspend-demo-nextjs/next.config.js
@@ -3,11 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["@b3dotfun/sdk"],
   env: {
-    NEXT_PUBLIC_THIRDWEB_CLIENT_ID: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID || "f393c7eb287696dc4db76d980cc68328",
-    NEXT_PUBLIC_THIRDWEB_ECOSYSTEM_ID: process.env.NEXT_PUBLIC_THIRDWEB_ECOSYSTEM_ID || "ecosystem.b3-open-gaming",
+    NEXT_PUBLIC_THIRDWEB_CLIENT_ID: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID || "eb17a5ec4314526d42fc567821aeb9a6",
     PUBLIC_GLOBAL_ACCOUNTS_PARTNER_ID:
-      process.env.PUBLIC_GLOBAL_ACCOUNTS_PARTNER_ID || "dbcd5e9b-564e-4ba0-91a0-becf0edabb61",
-    NEXT_PUBLIC_B3_API: "https://api.b3.fun",
+      process.env.PUBLIC_GLOBAL_ACCOUNTS_PARTNER_ID || "ceba2f84-45ff-4717-b3e9-0acf0d062abd",
+    NEXT_PUBLIC_THIRDWEB_ECOSYSTEM_ID: process.env.NEXT_PUBLIC_THIRDWEB_ECOSYSTEM_ID || "ecosystem.b3dotfun",
+    NEXT_PUBLIC_B3_API: process.env.NEXT_PUBLIC_B3_API || "https://b3-api-development.up.railway.app",
   },
 };
 
--- apps/anyspend-demo-vite/vite.config.ts
+++ apps/anyspend-demo-vite/vite.config.ts
@@ -1,8 +1,8 @@
 import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-import path from "path";
 
 const __dirname = path.resolve();
 
@@ -11,8 +11,10 @@ export default defineConfig({
   plugins: [react(), nodePolyfills(), viteCommonjs()],
   define: {
     "process.env": {
-      NEXT_PUBLIC_THIRDWEB_CLIENT_ID: "f393c7eb287696dc4db76d980cc68328",
-      NEXT_PUBLIC_THIRDWEB_ECOSYSTEM_ID: "ecosystem.b3-open-gaming",
+      NEXT_PUBLIC_THIRDWEB_CLIENT_ID: "eb17a5ec4314526d42fc567821aeb9a6",
+      PUBLIC_GLOBAL_ACCOUNTS_PARTNER_ID: "ceba2f84-45ff-4717-b3e9-0acf0d062abd",
+      NEXT_PUBLIC_THIRDWEB_ECOSYSTEM_ID: "ecosystem.b3dotfun",
+      PUBLIC_B3_API: "https://b3-api-development.up.railway.app"
     },
   },
   resolve: {
