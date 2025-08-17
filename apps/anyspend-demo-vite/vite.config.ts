820695191beecb5788655c2fe7c8a30fb5a28570
nhd98z <nhd98z@gmail.com>
2025-08-17 21:35:44 +0700

    Prettier

--- apps/anyspend-demo-vite/vite.config.ts
+++ apps/anyspend-demo-vite/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
       NEXT_PUBLIC_THIRDWEB_CLIENT_ID: "eb17a5ec4314526d42fc567821aeb9a6",
       PUBLIC_GLOBAL_ACCOUNTS_PARTNER_ID: "ceba2f84-45ff-4717-b3e9-0acf0d062abd",
       NEXT_PUBLIC_THIRDWEB_ECOSYSTEM_ID: "ecosystem.b3dotfun",
-      PUBLIC_B3_API: "https://b3-api-development.up.railway.app"
+      PUBLIC_B3_API: "https://b3-api-development.up.railway.app",
     },
   },
   resolve: {
