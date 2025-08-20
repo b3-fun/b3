58a6b5f9b04412c9fd8b53b561833bf67a2f0aef
Mitch Myburgh <mitchmyburgh@gmail.com>
2025-08-20 15:50:30 +0200

    Add useSiwe hook for authentication in React Native

---
+++ packages/sdk/src/global-account/react/hooks/useSiwe.native.tsx
@@ -0,0 +1,46 @@
+import app from "@b3dotfun/sdk/global-account/app";
+import debug from "@b3dotfun/sdk/shared/utils/debug";
+import { useCallback } from "react";
+import { Account } from "thirdweb/wallets";
+
+export function useSiwe() {
+
+  const authenticate = useCallback(
+    async (account: Account, partnerId: string) => {
+      if (!account || !account.signMessage) throw new Error("Account not found");
+
+      console.log("@@useAuthenticate:referrerId", referrerId);
+      // generate challenge
+      const challenge = await app.service("global-accounts-challenge").create({
+        address: account.address,
+      });
+      debug("@@useAuthenticate:challenge", challenge);
+
+      // sign challenge
+      const signature = await account.signMessage({
+        message: challenge.message,
+      });
+
+      debug("@@useAuthenticate:signature", signature);
+
+      // authenticate
+      const response = await app.authenticate({
+        strategy: "smart-account-siwe",
+        message: challenge.message,
+        signature,
+        serverSignature: challenge.serverSignature,
+        nonce: challenge.nonce,
+        // http://localhost:5173/?referrerId=cd8fda06-3840-43d3-8f35-ae9472a13759
+        partnerId: partnerId,
+      });
+      debug("@@useAuthenticate:response", response);
+
+      return response;
+    },
+    [],
+  );
+
+  return {
+    authenticate,
+  };
+}
