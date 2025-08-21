0376245af35e36cf201dd896de57f9390ecfad3e
An <anqtran36@gmail.com>
2025-08-21 19:37:47 -0400

    update abi

--- packages/sdk/src/bondkit/constants.ts
+++ packages/sdk/src/bondkit/constants.ts
@@ -1,5 +1,5 @@
 import type { Address } from "viem";
 
-export const BaseBondkitTokenFactoryContractAddress: Address = "0x66Bfa76731a280f8265ece3c8ec0953440a386aE";
+export const BaseBondkitTokenFactoryContractAddress: Address = "0x5d641bbB206d4B5585eCCd919F36270200A9A2Ad";
 
 export const BaseMainnetRpcUrl = "https://base-rpc.publicnode.com";
