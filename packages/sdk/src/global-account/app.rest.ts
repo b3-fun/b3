f78c15f7f60054faebbee245462e452dbdfd0ac2
Gio Martinez <gio@giologist.com>
2025-09-06 09:42:18 -0400

    correct exports

--- packages/sdk/src/global-account/app.rest.ts
+++ packages/sdk/src/global-account/app.rest.ts
@@ -1,12 +1,4 @@
-// Re-export from client manager for explicit REST client access
-export {
-  authenticateWithClient as authenticate,
-  getClientByType as default,
-  getClientByType,
-  getClientType,
-  setClientType,
-} from "./client-manager";
-
-// Helper to get REST client specifically
 import { getClientByType } from "./client-manager";
+
+export { authenticateWithClient, getClientByType, getClientType, setClientType } from "./client-manager";
 export const getRestClient = () => getClientByType("rest");
