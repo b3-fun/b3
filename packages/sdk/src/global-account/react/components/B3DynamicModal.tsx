9c75140eeb1d44f5a475bae3d83149d1087c0577
Gio Martinez <gio@giologist.com>
2025-09-06 07:19:40 -0400

    prettier

--- packages/sdk/src/global-account/react/components/B3DynamicModal.tsx
+++ packages/sdk/src/global-account/react/components/B3DynamicModal.tsx
@@ -68,8 +68,8 @@ export function B3DynamicModal() {
     contentType?.type === "signInWithB3" && "p-0",
     contentType?.type === "anySpend" && "md:px-6",
     // Add specific styles for avatar editor
-    contentType?.type === "avatarEditor_disabled" &&
-      "h-[90dvh] w-[90vw] bg-black p-0 overflow-y-auto overflow-x-hidden max-md:-mt-8 max-md:rounded-t-xl",
+    // contentType?.type === "avatarEditor_disabled" &&
+    //   "h-[90dvh] w-[90vw] bg-black p-0 overflow-y-auto overflow-x-hidden max-md:-mt-8 max-md:rounded-t-xl",
   );
 
   debug("contentType", contentType);
