640f8447642fdfa20f91a7aa98b8fdf1e8d519d0
Gio Martinez <gio@giologist.com>
2025-10-16 17:40:05 +0100

    New useTWAuth hook

---
+++ packages/sdk/src/global-account/react/hooks/useTWAuth.tsx
@@ -0,0 +1,34 @@
+import app from "@b3dotfun/sdk/global-account/app";
+import debug from "@b3dotfun/sdk/shared/utils/debug";
+import { useCallback } from "react";
+import { Wallet } from "thirdweb/wallets";
+import { useSearchParam } from "./useSearchParamsSSR";
+
+export function useTWAuth() {
+  const referralCode = useSearchParam("referralCode");
+
+  const authenticate = useCallback(
+    async (wallet: Wallet, partnerId: string) => {
+      if (!wallet || !wallet?.getAuthToken?.()) throw new Error("Wallet not found");
+
+      const authToken = wallet.getAuthToken();
+      debug("@@useTWSignIn:authToken", authToken);
+      debug("@@useTWSignIn:referralCode", referralCode);
+
+      // authenticate
+      const response = await app.authenticate({
+        strategy: "thirdweb-jwt",
+        accessToken: authToken,
+        partnerId: partnerId,
+      });
+      debug("@@useTWSignIn:response", response);
+
+      return response;
+    },
+    [referralCode],
+  );
+
+  return {
+    authenticate,
+  };
+}
