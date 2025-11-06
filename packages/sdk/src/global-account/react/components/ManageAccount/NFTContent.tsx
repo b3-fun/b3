6b603a7a7bad754cf27586447a13d68b8505835a
lubojanski <woooj23@gmail.com>
2025-11-06 15:08:58 +0000

    refactor: update NFTContent to use global wallet state and active wallet for account assets

--- packages/sdk/src/global-account/react/components/ManageAccount/NFTContent.tsx
+++ packages/sdk/src/global-account/react/components/ManageAccount/NFTContent.tsx
@@ -1,10 +1,16 @@
-import { useActiveAccount } from "thirdweb/react";
+import { useGlobalWalletState } from "@b3dotfun/sdk/anyspend/utils/accountStore";
+import { useActiveWallet } from "thirdweb/react";
 import { AccountAssets } from "..";
 import { useAccountAssets } from "../../hooks";
 
 const NFTContent = () => {
-  const account = useActiveAccount();
-  const { data: nfts, isLoading } = useAccountAssets(account?.address);
+  // Get active wallet state
+  const activeWallet = useActiveWallet();
+  const globalAccountWallet = useGlobalWalletState(state => state.globalAccountWallet);
+  const activeAccount = activeWallet?.getAccount();
+  const activeAddress = activeAccount?.address;
+
+  const { data: nfts, isLoading } = useAccountAssets(activeAddress);
 
   return (
     <div className="grid grid-cols-3 gap-4" style={{ minHeight: "100px" }}>
