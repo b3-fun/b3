43ac11b44ba701b756c3e1eca7d40d38da27eaf6
Gio Martinez <gio@giologist.com>
2025-12-16 23:32:21 +0900

    [B3-2839] Fix anyspend-demo-nextjs app

--- apps/anyspend-demo-nextjs/next.config.js
+++ apps/anyspend-demo-nextjs/next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   transpilePackages: ["@b3dotfun/sdk"],
   env: {
     NEXT_PUBLIC_THIRDWEB_CLIENT_ID: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID || "eb17a5ec4314526d42fc567821aeb9a6",
-    PUBLIC_GLOBAL_ACCOUNTS_PARTNER_ID:
-      process.env.PUBLIC_GLOBAL_ACCOUNTS_PARTNER_ID || "ceba2f84-45ff-4717-b3e9-0acf0d062abd",
+    NEXT_PUBLIC_GLOBAL_ACCOUNTS_PARTNER_ID:
+      process.env.NEXT_PUBLIC_GLOBAL_ACCOUNTS_PARTNER_ID || "ceba2f84-45ff-4717-b3e9-0acf0d062abd",
     NEXT_PUBLIC_THIRDWEB_ECOSYSTEM_ID: process.env.NEXT_PUBLIC_THIRDWEB_ECOSYSTEM_ID || "ecosystem.b3dotfun",
     NEXT_PUBLIC_B3_API: process.env.NEXT_PUBLIC_B3_API || "https://b3-api-development.up.railway.app",
     NEXT_PUBLIC_ANYSPEND_BASE_URL: process.env.NEXT_PUBLIC_ANYSPEND_BASE_URL || "https://mainnet.anyspend.com",
