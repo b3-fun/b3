677d47b63f0e8459cd5c3e6e1f111d9d4b73bc2c
nnti3n <thienphong215@gmail.com>
2025-12-30 00:05:54 +0700

    Remove setting global account as active on modal close. Anyspend already updates the selected wallet using useConnectModal with setActive: false.

--- packages/sdk/src/global-account/react/components/B3DynamicModal.tsx
+++ packages/sdk/src/global-account/react/components/B3DynamicModal.tsx
@@ -14,12 +14,10 @@ import { AnySpendDepositHype } from "@b3dotfun/sdk/anyspend/react/components/Any
 import { AnySpendDepositUpside } from "@b3dotfun/sdk/anyspend/react/components/AnySpendDepositUpside";
 import { AnySpendStakeUpside } from "@b3dotfun/sdk/anyspend/react/components/AnySpendStakeUpside";
 import { AnySpendStakeUpsideExactIn } from "@b3dotfun/sdk/anyspend/react/components/AnySpendStakeUpsideExactIn";
-import { useB3Config, useGlobalAccount, useIsMobile, useModalStore } from "@b3dotfun/sdk/global-account/react";
+import { useB3Config, useIsMobile, useModalStore } from "@b3dotfun/sdk/global-account/react";
 import { cn } from "@b3dotfun/sdk/shared/utils/cn";
 import { debugB3React } from "@b3dotfun/sdk/shared/utils/debug";
 import { AnimatePresence, motion } from "framer-motion";
-import { useEffect, useRef } from "react";
-import { useSetActiveWallet } from "thirdweb/react";
 import { AvatarEditor } from "./AvatarEditor/AvatarEditor";
 import { Deposit } from "./Deposit/Deposit";
 import { LinkAccount } from "./LinkAccount/LinkAccount";
@@ -43,21 +41,7 @@ export function B3DynamicModal() {
   const navigateBack = useModalStore(state => state.navigateBack);
   const { theme } = useB3Config();
   const isMobile = useIsMobile();
-  const prevIsOpenRef = useRef(isOpen);
-  const { wallet } = useGlobalAccount();
-  const setActiveWallet = useSetActiveWallet();
   const { toasts, removeToast } = useToastContext();
-
-  // anyspend cleanup global account chnages by setting account back
-  useEffect(() => {
-    if (prevIsOpenRef.current && !isOpen) {
-      if (wallet) {
-        setActiveWallet(wallet);
-      }
-    }
-
-    prevIsOpenRef.current = isOpen;
-  }, [isOpen, wallet, setActiveWallet]);
 
   // Define arrays for different modal type groups
   const fullWidthTypes = [
