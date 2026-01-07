72a063d75d93eb35c196f2aa44f2eec53cdb8a9d
nnti3n <thienphong215@gmail.com>
2026-01-07 21:02:27 +0700

    Add backward compatible dark theme support
    Support multiple dark theme selectors:
    - html.dark .b3-root (next-themes with class attribute)
    - html[data-theme="dark"] .b3-root (next-themes with data attribute)
    - .b3-root[data-theme="dark"] (B3Provider theme prop)
    - .b3-root.dark (manual dark class)

--- packages/sdk/src/global-account/react/components/StyleRoot.tsx
+++ packages/sdk/src/global-account/react/components/StyleRoot.tsx
@@ -1,8 +1,11 @@
 import { PropsWithChildren } from "react";
+import { useB3Config } from "./B3Provider/useB3Config";
 
 export function StyleRoot({ children, id }: PropsWithChildren<{ id?: string }>) {
+  const { theme } = useB3Config();
+
   return (
-    <div className="b3-root" id={id}>
+    <div className="b3-root" id={id} data-theme={theme}>
       {children}
     </div>
   );
